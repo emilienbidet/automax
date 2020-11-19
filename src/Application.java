d446208a6f3ee38906deab977b2930aa523c130f
emilienbidet <emilienbdt@gmail.com>
2020-11-19 19:31:41 +0100

    add automate test

--- src/Application.java
+++ src/Application.java
@@ -1,5 +1,7 @@
+import java.io.IOException;
+
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         State e0 = new State("E0");
         State h1 = new State("H1");
         State h2 = new State("H2");
@@ -9,11 +11,19 @@ public class Application {
         State m = new State("M");
 
         e0.addTransition('0', h1);
+        e0.addTransition('1', h1);
+        e0.addTransition('2', h2);
         // etc...
 
-        Automate hhmm = new Automate("HH:MM", e0, m);
-        hhmm.addState(e0);
-        hhmm.addState(h1);
+        Automate automateTest = new Automate("HH:MM", e0, m);
+        automateTest.addState(e0);
+        automateTest.addState(h1);
+        //etc..
+
+
+        Automate hhmmm = Automate.createAutomateFromFile("hhmm.csv", "HH:MM", ',', "#");
+
+
 
     }
 }
