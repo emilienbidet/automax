c9d1beb635c1aa2fd1e789ea84cc445d9f428766
emilienbidet <emilienbdt@gmail.com>
2020-11-21 01:20:09 +0100

    comment all methods of Automate class

--- src/Automate.java
+++ src/Automate.java
@@ -11,6 +11,12 @@ public class Automate {
     private State finalState;
     private ArrayList<State> delta;
 
+    /**
+     * Method to declare a new automate
+     * @param name name of the automate
+     * @param initialState initial state of the automate
+     * @param finalState final state of the automate
+     */
     public Automate(String name, State initialState, State finalState) {
         this.name = name;
         this.initialState = initialState;
@@ -18,34 +24,54 @@ public class Automate {
         this.delta = new ArrayList<>();
     }
 
+    /**
+     * Method to add state to the current delta
+     * @param state state to add
+     */
     public void addState(State state) {
-        delta.add(state);
+        this.delta.add(state);
     }
 
+    /**
+     * Method to add all states to the curent delta
+     * @param states states to add
+     */
     public void addAllStates(ArrayList<State> states) {
-        delta.addAll(states);
+        this.delta.addAll(states);
     }
 
+    /**
+     * Method to copy the current Automate
+     * @return copy of the current automate
+     */
+    public Automate copy() {
+        return new Automate(this.name, this.initialState.copy(), this.finalState.copy());
+    }
+
+    /**
+     * Method to veify if s is accepted by the automate
+     * @param s string to verify
+     * @return true if if s is accepted by the automate
+     */
     public boolean verify(String s) {
         State currentState = initialState;
 
         char[] word = s.toCharArray();
 
         for (int i = 0; i < word.length; i++) {
-            currentState = currentState.getTransition(word[i]);
-            System.out.println(currentState);
+            currentState = currentState.getNextState(String.valueOf(word[i]));
+            System.out.println(currentState); // Watch the path
             if (currentState == null) {
                 return false;
             }
         }
-        return true;
+        return currentState.equals(this.finalState) || currentState.getNextState("").equals(this.finalState);
     }
-
 
     /**
      * Method to create an Automate from the file given.
-     * @param file the name of the file
-     * @param name the name of the automate
+     * @param file name of the file
+     * @param name name of the automate
      * @param separator the separator of the file
      *                  ex: ',' for csv files
      * @param transitionSeparator the separator of transitions
@@ -81,7 +107,7 @@ public class Automate {
                         transState = s;
                     }
                 }
-                state.addTransition(buffer[0].charAt(0), transState);
+                state.addTransition(buffer[0], transState);
             }
             lineCounter++;
         }
