b1e4d5b4a2aa2df9b35fc928e580da71cf00dd88
emilienbidet <emilienbdt@gmail.com>
2020-12-03 22:51:28 +0100

    correct misstake

--- src/Application.java
+++ src/Application.java
@@ -7,40 +7,40 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) throws IOException, ParseException {
 
-        // Add automates to the list
-        ArrayList<Automate> automates = new ArrayList<>();
-        automates.add(Automate.createAutomateFromCSVFile("automates/csv/hhmm.csv", ',', "#"));
-        automates.add(Automate.createAutomateFromCSVFile("automates/csv/smileys.csv", ',', "#"));
-        automates.add(Automate.createAutomateFromCSVFile("automates/csv/jjmmaa.csv", ',', "#"));
-        automates.add(Automate.createAutomateFromCSVFile("automates/csv/hhmmss.csv", ',', "#"));
-        automates.add(Automate.createAutomateFromCSVFile("automates/csv/email.csv", ',', "#"));
-        automates.add(Automate.createAutomateFromCSVFile("automates/csv/emailList.csv", ',', "#"));
-        automates.add(Automate.createAutomateFromCSVFile("automates/csv/polynomials.csv", ',', "#"));
+        // Add automatons to the list
+        ArrayList<Automate> automatons = new ArrayList<>();
+        automatons.add(Automate.createAutomateFromCSVFile("automatons/csv/hhmm.csv", ',', "#"));
+        automatons.add(Automate.createAutomateFromCSVFile("automatons/csv/smileys.csv", ',', "#"));
+        automatons.add(Automate.createAutomateFromCSVFile("automatons/csv/jjmmaa.csv", ',', "#"));
+        automatons.add(Automate.createAutomateFromCSVFile("automatons/csv/hhmmss.csv", ',', "#"));
+        automatons.add(Automate.createAutomateFromCSVFile("automatons/csv/email.csv", ',', "#"));
+        automatons.add(Automate.createAutomateFromCSVFile("automatons/csv/emailList.csv", ',', "#"));
+        automatons.add(Automate.createAutomateFromCSVFile("automatons/csv/polynomials.csv", ',', "#"));
 
-        // Uncomment to test automates from tp with different types of files
-        // testCsvAutomates();
-        // testJsonAutomates();
+        // Uncomment to test automatons from tp with different types of files
+        // testCsvautomatons();
+        // testJsonautomatons();
 
         // Run the application
-        runApplication(automates);
+        runApplication(automatons);
     }
 
     /**
      * Method to run the application
-     * @param automates automates to list
+     * @param automatons automatons to list
      */
-    public static void runApplication(ArrayList<Automate> automates) {
+    public static void runApplication(ArrayList<Automate> automatons) {
         // Get the user's choice
-        int choice = getAutomateChoice(getMainMenu(automates), 1, automates.size() + 1);
+        int choice = getAutomateChoice(getMainMenu(automatons), 1, automatons.size() + 1);
         String stringToAnalyse = "";
         Automate currentAutomate;
         // Analyse the string with the choosed automate
         // Stop the application if the user ask
-        while (choice != automates.size() + 1) {
-            currentAutomate = automates.get(choice - 1);
+        while (choice != automatons.size() + 1) {
+            currentAutomate = automatons.get(choice - 1);
             stringToAnalyse = getStringToAnalyse(getAutomateMenu(currentAutomate));
             System.out.println("Résultat : " + (currentAutomate.verify(stringToAnalyse) ? "OK": "KO"));
-            choice = getAutomateChoice(getMainMenu(automates), 1, automates.size() + 1);
+            choice = getAutomateChoice(getMainMenu(automatons), 1, automatons.size() + 1);
         }
         System.out.println("Bye.");
     }
@@ -85,16 +85,16 @@ public class Application {
     /**
      * Method to retunr the tp's menu
      * For each line you have a different automate
-     * @param automates automates
+     * @param automatons automatons
      * @return main menu
      */
-    public static String getMainMenu(ArrayList<Automate> automates){
+    public static String getMainMenu(ArrayList<Automate> automatons){
         String menu = "";
         menu += "---------------- Menu de mon TP ----------------\n";
-        for (Automate automate : automates) {
-            menu += (automates.indexOf(automate) + 1) + " - " + automate.getName() + "\n";
+        for (Automate automate : automatons) {
+            menu += (automatons.indexOf(automate) + 1) + " - " + automate.getName() + "\n";
         }
-        menu += (automates.size() + 1) + " - Arrêt\n";
+        menu += (automatons.size() + 1) + " - Arrêt\n";
         menu += "Votre choix ?\n";
         menu += "Je vous demanderais ensuite la chaîne à analyser. Merci\n";
         menu += "------------------------------------------------";
@@ -162,47 +162,47 @@ public class Application {
     }
 
     /**
-     * Method to test tp's automates with csv files
+     * Method to test tp's automatons with csv files
      * @throws IOException the file can't be find
      */
-    public static void testCsvAutomates() throws IOException {
+    public static void testCsvAutomatons() throws IOException {
         // HH:MM automate
-        Automate hhmm = Automate.createAutomateFromCSVFile("automates/csv/hhmm.csv", ',', "#");
+        Automate hhmm = Automate.createAutomateFromCSVFile("automatons/csv/hhmm.csv", ',', "#");
         System.out.println(hhmm.verify("21:30")); // Time to sleep
 
         // Smiley automate
-        Automate smileys = Automate.createAutomateFromCSVFile("automates/csv/smileys.csv", ',', "#");
+        Automate smileys = Automate.createAutomateFromCSVFile("automatons/csv/smileys.csv", ',', "#");
         System.out.println(smileys.verify(":-)")); // Be happy
 
         // JJ/MM/AAAA automate
-        Automate jjmmaa = Automate.createAutomateFromCSVFile("automates/csv/jjmmaa.csv", ',', "#");
+        Automate jjmmaa = Automate.createAutomateFromCSVFile("automatons/csv/jjmmaa.csv", ',', "#");
         System.out.println(jjmmaa.verify("02/07/2052")); // There may be aliens 👽
 
         // HH:MM:SS automate
-        Automate hhmmss = Automate.createAutomateFromCSVFile("automates/csv/hhmmss.csv", ',', "#");
+        Automate hhmmss = Automate.createAutomateFromCSVFile("automatons/csv/hhmmss.csv", ',', "#");
         System.out.println(hhmmss.verify("16:00:00")); // Time to snack ^^
 
         // email automate
-        Automate email = Automate.createAutomateFromCSVFile("automates/csv/email.csv", ',', "#");
+        Automate email = Automate.createAutomateFromCSVFile("automatons/csv/email.csv", ',', "#");
         System.out.println(email.verify("emilien-bidet@gmail.com")); // Secret agent email
 
         // email list automate
-        Automate emailList = Automate.createAutomateFromCSVFile("automates/csv/emailList.csv", ',', "#");
+        Automate emailList = Automate.createAutomateFromCSVFile("automatons/csv/emailList.csv", ',', "#");
         System.out.println(emailList.verify("emilien-bidet@gmail.com;aRealSpy@gouv.fr")); // Secret agent emails
 
         // polynomials
-        Automate polynomials = Automate.createAutomateFromCSVFile("automates/csv/polynomials.csv", ',', "#");
+        Automate polynomials = Automate.createAutomateFromCSVFile("automatons/csv/polynomials.csv", ',', "#");
         System.out.println(polynomials.verify("14X**2 + 25X - 25")); // Love potion formula
     }
 
     /**
-     * Method to test tp's automates with json files
+     * Method to test tp's automatons with json files
      * @throws IOException the file can't be find
      * @throws ParseException the file is not build properly
      */
-    public static void testJsonAutomates() throws IOException, ParseException {
+    public static void testJsonAutomatons() throws IOException, ParseException {
         // HH:MM automate
-        Automate hhmm = Automate.createAutomateFromJsonFile("automates/json/hhmm.json");
+        Automate hhmm = Automate.createAutomateFromJsonFile("automatons/json/hhmm.json");
         System.out.println(hhmm.verify("21:30")); // Time to sleep
     }
 }
